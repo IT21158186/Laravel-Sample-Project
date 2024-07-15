5c0a79886147ba87179f4dccf495d190c4efcf60
IT21158186 <it21158186@my.sliit.lk>
2024-07-16 00:48:53 +0530

    Initialize my new Laravel project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <h1>My First Laravel View</h1>
+</head>
+
+<body>
+    <h1>Welcome to My First Laravel View!</h1>
+    <p>This is a simple Laravel application.</p>
+    <p>Current time: {{ date('Y-m-d H:i:s') }}</p>
+</body>
+</html>
